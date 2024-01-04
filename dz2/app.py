87aee089226a2d73e0a90a154e0cdb2dec64e531
Vano0928 <vanyapro57@gmail.com>
2024-01-05 00:24:00 +0200

    a

--- dz2/app.py
+++ dz2/app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates", static_folder="static")
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/home')
+def home():
+    return render_template("home.html", title = "index")
+
+
+@app.route('/contacts')
+def contacts():
+    return render_template("contacts.html", title = "Контакти")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html", title = "Про нас")
 
 
 if __name__ == '__main__':
